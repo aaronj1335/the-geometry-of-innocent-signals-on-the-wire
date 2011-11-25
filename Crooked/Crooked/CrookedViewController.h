8d3c172dc7b1cd3565e49e53d57109e4bfb4c3c6
Aaron Stacy <aaron.r.stacy@gmail.com>
2011-11-25 12:24:54 -0600

    initial commit

---
+++ Crooked/Crooked/CrookedViewController.h
@@ -0,0 +1,13 @@
+//
+//  CrookedViewController.h
+//  Crooked
+//
+//  Created by Aaron Stacy on 11/25/11.
+//  Copyright (c) 2011 StoredIQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CrookedViewController : UIViewController
+
+@end
---
+++ Crooked/Crooked/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Crooked
+//
+//  Created by Aaron Stacy on 11/25/11.
+//  Copyright (c) 2011 StoredIQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CrookedAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CrookedAppDelegate class]));
+    }
+}
