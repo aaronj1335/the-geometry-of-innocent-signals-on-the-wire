15144bdc84f8c815f18e4bd82b423f254bca8f47
Aaron Stacy <aaron.r.stacy@gmail.com>
2011-11-25 13:40:17 -0600

    added record/play functionality

--- Crooked/Crooked/CrookedViewController.h
+++ Crooked/Crooked/CrookedViewController.h
@@ -7,7 +7,32 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
 
-@interface CrookedViewController : UIViewController
+@interface CrookedViewController : UIViewController <AVAudioPlayerDelegate> {
+    AVAudioPlayer* player;
+    AVAudioRecorder* recorder;
+}
+
+@property (weak, nonatomic) IBOutlet UIButton* recordButton;
+
+@property (weak, nonatomic) IBOutlet UIButton* playButton;
+
+@property (copy, nonatomic) NSURL* url;
+
+@property (strong, nonatomic) NSMutableDictionary* recordSettings;
+
+- (IBAction)record:(id)sender;
+
+- (void)startRecord;
+
+- (void)stopRecord;
+
+- (IBAction)play:(id)sender;
+
+- (void)startPlay;
+
+- (void)stopPlay;
+- (void)audioPlayerDidFinishPlaying:(AVAudioPlayer*)plyr successfully:(BOOL)flag;
 
 @end
--- Crooked/Crooked/CrookedViewController.m
+++ Crooked/Crooked/CrookedViewController.m
@@ -9,6 +9,10 @@
 #import "CrookedViewController.h"
 
 @implementation CrookedViewController
+@synthesize recordButton;
+@synthesize playButton;
+@synthesize url = _url;
+@synthesize recordSettings = _recordSettings;
 
 - (void)didReceiveMemoryWarning
 {
@@ -22,13 +26,30 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.url = [NSURL fileURLWithPath:[NSString stringWithFormat:@"%@/recorded_audio.pcm", [[NSBundle mainBundle] resourcePath]]];
+    
+    self.recordSettings = [[NSMutableDictionary alloc] initWithCapacity:10];
+    [self.recordSettings setObject:[NSNumber numberWithInt:kAudioFormatLinearPCM] forKey:AVFormatIDKey];
+    [self.recordSettings setObject:[NSNumber numberWithFloat:44100.0] forKey:AVSampleRateKey];
+    [self.recordSettings setObject:[NSNumber numberWithInt:2] forKey:AVNumberOfChannelsKey];
+    [self.recordSettings setObject:[NSNumber numberWithInt:16] forKey:AVLinearPCMBitDepthKey];
+    [self.recordSettings setObject:[NSNumber numberWithBool:NO] forKey:AVLinearPCMIsBigEndianKey];
+    [self.recordSettings setObject:[NSNumber numberWithBool:NO] forKey:AVLinearPCMIsFloatKey];
 }
 
 - (void)viewDidUnload
 {
+    [self setRecordButton:nil];
+    [self setPlayButton:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
+    
+    player = nil;
+    recorder = nil;
+    self.url = nil;
+    self.recordSettings = nil;
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -55,6 +76,79 @@
 {
     // Return YES for supported orientations
     return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+- (IBAction)record:(id)sender {
+    NSLog(@"--------------------------------------- record pressed");
+    NSLog(@"recording to %@", [self.url absoluteURL]);
+    NSLog(@"record settings: %@", [self.recordSettings description]);
+    
+    if (recorder == nil) {
+        [self startRecord];
+        [recordButton setTitle:@"Stop Recording" forState:UIControlStateNormal];
+        [playButton setTitle:@"Stop Recording and Play" forState:UIControlStateNormal];
+    } else {
+        [self stopRecord];
+        [recordButton setTitle:@"Record" forState:UIControlStateNormal];
+        [playButton setTitle:@"Play" forState:UIControlStateNormal];
+    }
+}
+
+- (void)startRecord {
+    AVAudioSession* session = [AVAudioSession sharedInstance];
+    [session setCategory:AVAudioSessionCategoryPlayback error:nil];
+
+    NSError __autoreleasing* error = nil;
+    recorder = [[AVAudioRecorder alloc] initWithURL:self.url settings:self.recordSettings error:&error];
+    
+    if ([recorder prepareToRecord] == YES) {
+        [recorder record];
+    } else {
+        int errorCode = CFSwapInt32HostToBig([error code]);
+        NSLog(@"ERROR: %@ [%4.4s])", [error localizedDescription], (char*)&errorCode);
+    }
+    
+    NSLog(@"recording");
+}
+
+- (void)stopRecord {
+    [recorder stop];
+    recorder = nil;
+}
+
+- (IBAction)play:(id)sender {
+    NSLog(@"--------------------------------------- play pressed");
+    if (recorder != nil)
+        [self record:recordButton];
+
+    if (player == nil) {
+        [self startPlay];
+        [playButton setTitle:@"Stop Playing" forState:UIControlStateNormal];
+    } else {
+        [self stopPlay];
+        [playButton setTitle:@"Play" forState:UIControlStateNormal];        
+    }
+}
+
+- (void)startPlay {
+    AVAudioSession* session = [AVAudioSession sharedInstance];
+    [session setCategory:AVAudioSessionCategoryRecord error:nil];
+    
+    NSError __autoreleasing* error;
+    player = [[AVAudioPlayer alloc] initWithContentsOfURL:self.url error:&error];
+    [player setDelegate:self];
+    [player play];
+    
+    NSLog(@"playaz gonna play");
+}
+
+- (void)stopPlay {
+    [player stop];
+    player = nil;
+}
+
+- (void)audioPlayerDidFinishPlaying:(AVAudioPlayer*)plyr successfully:(BOOL)flag {
+    [self play:playButton];
 }
 
 @end
